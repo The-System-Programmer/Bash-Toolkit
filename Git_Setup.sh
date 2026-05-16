00ee2daa5addb971028d0a8dda116082966b522e
Rohit <rohit.haridas.dev@gmail.com>
2026-05-16 10:31:58 +0530

    Shell program to setup github and git with ssh in linux based operating system

---
+++ Git_Setup.sh
@@ -0,0 +1,14 @@
+echo "This script is to setup git and connect it with github using ssh"
+read -p "Enter your user name : " name
+git config --global user.name $name
+read -p "Enter your user email : " email
+git config --global user.emai $email
+git config --list
+echo "Your configured successfully"
+
+ssh-keygen -t ed25519 -C "$email"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "ssh generated successfully"
+echo "add this ssh key in github : "
+cat ~/.ssh/id_ed25519.pub
